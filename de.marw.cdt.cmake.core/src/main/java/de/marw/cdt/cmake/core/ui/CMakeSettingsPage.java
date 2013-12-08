e0432fce7ec49500b709d43155eaaa8e92a175aa
fifteenknots505 <fifteenknots505@gmail.com>
2013-12-08 22:23:22 +0100

    added pages for cmake preferences aund settings

---
+++ de.marw.cdt.cmake.core/src/main/java/de/marw/cdt/cmake/core/ui/CMakeSettingsPage.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Martin Weber.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Martin Weber - Initial implementation
+ *******************************************************************************/
+package de.marw.cdt.cmake.core.ui;
+
+import org.eclipse.cdt.ui.newui.AbstractPrefPage;
+
+/**
+ * Page for CMake project settings.
+ *
+ * @author Martin Weber
+ */
+public class CMakeSettingsPage extends AbstractPrefPage {
+
+  /*-
+   * @see org.eclipse.cdt.ui.newui.AbstractPrefPage#getHeader()
+   */
+  @Override
+  protected String getHeader() {
+    return isSingle() ? null : "CMake settings";
+  }
+
+  /*-
+   * @see org.eclipse.cdt.ui.newui.AbstractPage#isSingle()
+   */
+  @Override
+  protected boolean isSingle() {
+    return true;// currently a single Tab
+  }
+
+}
---
+++ de.marw.cdt.cmake.core/src/main/java/de/marw/cdt/cmake/core/ui/CMakeSettingsTab.java
@@ -0,0 +1,60 @@
+// Copyright 2013 Martin Weber
+
+package de.marw.cdt.cmake.core.ui;
+
+import org.eclipse.cdt.core.settings.model.ICResourceDescription;
+import org.eclipse.cdt.ui.newui.AbstractCPropertyTab;
+
+/**
+ * @author Martin Weber
+ */
+public class CMakeSettingsTab extends
+//org.eclipse.cdt.ui.newui.BinaryParsTab {
+AbstractCPropertyTab {
+
+  /**
+   *
+   */
+  public CMakeSettingsTab() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /*-
+   * @see org.eclipse.cdt.ui.newui.AbstractCPropertyTab#performApply(org.eclipse.cdt.core.settings.model.ICResourceDescription, org.eclipse.cdt.core.settings.model.ICResourceDescription)
+   */
+  @Override
+  protected void performApply(ICResourceDescription src,
+      ICResourceDescription dst) {
+    // TODO Auto-generated function stub
+
+  }
+
+  /*-
+   * @see org.eclipse.cdt.ui.newui.AbstractCPropertyTab#performDefaults()
+   */
+  @Override
+  protected void performDefaults() {
+    // TODO Auto-generated function stub
+
+  }
+
+  /*-
+   * @see org.eclipse.cdt.ui.newui.AbstractCPropertyTab#updateData(org.eclipse.cdt.core.settings.model.ICResourceDescription)
+   */
+  @Override
+  protected void updateData(ICResourceDescription cfg) {
+    // TODO Auto-generated function stub
+
+  }
+
+  /*-
+   * @see org.eclipse.cdt.ui.newui.AbstractCPropertyTab#updateButtons()
+   */
+  @Override
+  protected void updateButtons() {
+    // TODO Auto-generated function stub
+
+  }
+
+}
