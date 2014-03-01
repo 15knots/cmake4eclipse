f2d1f81d3f500543dd4622b09fb19dc1b1623a98
15knots <15knots@users.noreply.github.com>
2014-03-01 21:40:39 +0100

    Build uses target definition instead of p2 repos

--- de.marw.cdt.cmake.core/src/main/java/de/marw/cdt/cmake/core/ui/AbstractOsPropertyTab.java
+++ de.marw.cdt.cmake.core/src/main/java/de/marw/cdt/cmake/core/ui/AbstractOsPropertyTab.java
@@ -518,8 +518,9 @@ public abstract class AbstractOsPropertyTab<P extends AbstractOsPreferences>
    * Updates displayed values according to the preferences edited by this tab.
    */
   private void updateDisplay() {
-    t_cmd.setText(prefs.getCommand());
     b_cmdFromPath.setSelection(prefs.getUseDefaultCommand());
+    t_cmd.setEnabled(!prefs.getUseDefaultCommand());
+    t_cmd.setText(prefs.getCommand());
 
     String generatorName = prefs.getGeneratorName();
     int idx = c_generator.indexOf(generatorName);
