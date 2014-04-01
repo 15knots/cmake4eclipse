c2f9fcea7cc337541d23926dbb3b703666d64781
fifteenknots505 <fifteenknots505@gmail.com>
2014-04-01 21:03:48 +0200

    clean up

--- de.marw.cdt.cmake.core/src/main/java/de/marw/cdt/cmake/core/ui/AbstractOsPropertyTab.java
+++ de.marw.cdt.cmake.core/src/main/java/de/marw/cdt/cmake/core/ui/AbstractOsPropertyTab.java
@@ -16,8 +16,6 @@ import java.util.List;
 import org.eclipse.cdt.core.settings.model.ICConfigurationDescription;
 import org.eclipse.cdt.core.settings.model.ICResourceDescription;
 import org.eclipse.cdt.core.settings.model.ICStorageElement;
-import org.eclipse.cdt.managedbuilder.core.IToolChain;
-import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
 import org.eclipse.cdt.ui.newui.AbstractCPropertyTab;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.ILog;
