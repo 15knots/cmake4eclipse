ca6ac3fc8a7e98d6a29ed76688efd799acbb56c5
15knots <15knots@users.noreply.github.com>
2016-11-06 15:58:30 +0100

    #20 CMAKE_EXPORT_COMPILE_COMMANDS Parser treats clang++ as a C compiler
    Added unitttest to verify

---
+++ de.marw.cmake.tests/src/main/java/de/marw/cmake/cdt/language/settings/providers/CompileCommandsJsonParserTest.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Martin Weber.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Martin Weber - Initial implementation
+ *******************************************************************************/
+package de.marw.cmake.cdt.language.settings.providers;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.marw.cmake.cdt.language.settings.providers.CompileCommandsJsonParser.ParserLookupResult;
+
+/**
+ * @author Martin Weber
+ */
+public class CompileCommandsJsonParserTest {
+
+  private CompileCommandsJsonParser testee;
+
+  @Before
+  public void setUp() {
+    testee = new CompileCommandsJsonParser();
+  }
+
+  /**
+   * Test method for
+   * {@link de.marw.cmake.cdt.language.settings.providers.CompileCommandsJsonParser#determineParserForCommandline(java.lang.String)}
+   * .
+   */
+  @Test
+  public void testDetermineParserForCommandline_clang() {
+    ParserLookupResult result = testee.determineParserForCommandline("/usr/bin/clang -C blah.c");
+    assertNotNull(result);
+    // verify that we got a C parser
+    assertEquals("C", "org.eclipse.cdt.core.gcc", result.parser.getLanguageId());
+  }
+
+  @Test
+  public void testDetermineParserForCommandline_clangplusplus() {
+    ParserLookupResult result = testee.determineParserForCommandline("/usr/bin/clang++ -C blah.c");
+    assertNotNull(result);
+    // verify that we got a C++ parser
+    assertEquals("C++", "org.eclipse.cdt.core.g++", result.parser.getLanguageId());
+  }
+}
