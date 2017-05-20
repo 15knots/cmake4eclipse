46ff10bf4e104a4d9439edec06adebbd03ebcce8
15knots <fifteenknots505@gmail.com>
2017-05-20 19:26:00 +0200

    integrate workaround for #31 into existing settings provider.

--- de.marw.cmake.tests/src/main/java/de/marw/cmake/cdt/language/settings/providers/CompileCommandsJsonParserTest.java
+++ de.marw.cmake.tests/src/main/java/de/marw/cmake/cdt/language/settings/providers/CompileCommandsJsonParserTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import de.marw.cmake.cdt.language.settings.providers.CompileCommandsJsonParser.ParserLookupResult;
@@ -49,5 +50,18 @@ public class CompileCommandsJsonParserTest {
     assertNotNull(result);
     // verify that we got a C++ parser
     assertEquals("C++", "org.eclipse.cdt.core.g++", result.parser.getLanguageId());
+  }
+
+  /**
+   * Test method for
+   * {@link de.marw.cmake.cdt.language.settings.providers.CompileCommandsJsonParser#determineParserForCommandline(java.lang.String)}
+   */
+  @Test
+  @Ignore("Requires NFTS to run")
+  public void testDetermineParserForCommandline_MsdosShortNames() {
+    ParserLookupResult result = testee.determineParserForCommandline("C:\\PROGRA2\\Atmel\\AVR8-G1\\bin\\AVR-G_~1.EXE -C blah.c");
+    assertNotNull(result);
+    // verify that we got a C parser
+    assertEquals("C", "org.eclipse.cdt.core.gcc", result.parser.getLanguageId());
   }
 }
