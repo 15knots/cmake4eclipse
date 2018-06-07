aeb0fc3da9fc65edb8f87174324b638c257d9ebc
15knots <15knots@users.noreply.github.com>
2018-06-07 21:08:26 +0200

    keep the compiler command

--- de.marw.cmake.tests/src/main/java/de/marw/cmake/cdt/language/settings/providers/ParserLookupResultTest.java
+++ de.marw.cmake.tests/src/main/java/de/marw/cmake/cdt/language/settings/providers/ParserLookupResultTest.java
@@ -67,4 +67,33 @@ public class ParserLookupResultTest {
     assertEquals("reducedCommandLine", args, result.getReducedCommandLine());
   }
 
+  /**
+   * Test method for
+   * {@link de.marw.cmake.cdt.language.settings.providers.ParserDetection.ParserDetectionResult#getCommandLine()}.
+   */
+  @Test
+  public void testGetCommandLine() {
+    String compiler = "/bin/c++";
+    String args = "-DQT_CORE_LIB -I/home/self/shared/qt5-project/build/Debug"
+        + " -isystem /home/self/Qt5.9.1/5.9.1/gcc_64/include/QtWidgets"
+        + " -g -fPIC -std=gnu++11 -o CMakeFiles/foo.dir/foo_automoc.cpp.o"
+        + " -c /home/self/shared/qt5-project/build/Debug/foo_automoc.cpp";
+    String cmd = compiler + " " + args;
+    ParserDetection.ParserDetectionResult result = ParserDetection.determineDetector(cmd, null, false);
+    assertNotNull(result);
+    // verify that we got a C++ parser
+    assertEquals("C++", "org.eclipse.cdt.core.g++", result.getDetectorWithMethod().getDetector().getParser().getLanguageId());
+    assertEquals("command", compiler, result.getCommandLine().getCommand());
+    assertEquals("args", args, result.getCommandLine().getArguments());
+
+    // test without leading path
+    compiler = "c++";
+    cmd = compiler + " " + args;
+    result = ParserDetection.determineDetector(cmd, null, false);
+    assertNotNull(result);
+    // verify that we got a C++ parser
+    assertEquals("C++", "org.eclipse.cdt.core.g++", result.getDetectorWithMethod().getDetector().getParser().getLanguageId());
+    assertEquals("command", compiler, result.getCommandLine().getCommand());
+    assertEquals("args", args, result.getCommandLine().getArguments());
+  }
 }
