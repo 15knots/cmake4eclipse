a9752e0ba57efe36b2d983d58b71b62ad57d441e
15knots <15knots@users.noreply.github.com>
2020-02-05 21:13:54 +0100

    Switch to EPL 2.0
    Fixes #130
    [skip ci]

--- de.marw.cdt.cmake.core/src/main/java/de/marw/cdt/cmake/core/ui/CMakePropertyPage.java
+++ de.marw.cdt.cmake.core/src/main/java/de/marw/cdt/cmake/core/ui/CMakePropertyPage.java
@@ -1,12 +1,10 @@
 /*******************************************************************************
  * Copyright (c) 2013 Martin Weber.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *      Martin Weber - Initial implementation
+ * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
+ * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 package de.marw.cdt.cmake.core.ui;
 
--- de.marw.cmake.cdt.lsp.intel/src/main/java/de/marw/cmake/cdt/lsp/intel/IntelCToolCommandlineParser.java
+++ de.marw.cmake.cdt.lsp.intel/src/main/java/de/marw/cmake/cdt/lsp/intel/IntelCToolCommandlineParser.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Martin Weber.
+ *
+ * Content is provided to you under the terms and conditions of the Eclipse Public License Version 2.0 "EPL".
+ * A copy of the EPL is available at http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
 package de.marw.cmake.cdt.lsp.intel;
 
 import de.marw.cmake.cdt.lsp.Arglets;
