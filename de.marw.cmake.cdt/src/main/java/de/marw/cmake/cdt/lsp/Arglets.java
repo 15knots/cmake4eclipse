3d5c3755e1c752a9411dfa4eec3decf5f5ed1a4f
ghha <Ghaith.HACHEM@bachmann.info>
2020-11-16 08:45:37 +0100

    #145 Add support for clang targets
    Addedf support for the --target flag used by clang to enable cross
    compilation

--- de.marw.cmake.cdt/src/main/java/de/marw/cmake/cdt/lsp/Arglets.java
+++ de.marw.cmake.cdt/src/main/java/de/marw/cmake/cdt/lsp/Arglets.java
@@ -43,6 +43,12 @@ public class Arglets {
    * matches an include path with unquoted directory. Name in matcher group 1
    */
   private static final String REGEX_INCLUDEPATH_UNQUOTED_DIR = "\\s*([^\\s]+)";
+  
+  /**
+   * matches a target triple definition
+   */
+  private static final String REGEX_TARGET_DEFNITION = "\\w+(-\\w+)*";
+
 
   /**
    * nothing to instantiate
@@ -495,6 +501,24 @@ public class Arglets {
       return processArgument(parseContext, argsLine, optionMatchers);
     }
   }
+  
+	////////////////////////////////////////////////////////////////////
+	/**
+	 * A tool argument parser capable to parse a Clang option to specify the compilation target {@code --sysrooot}.
+	 */
+	public static class Target_Clang extends BuiltinDetectionArgsGeneric implements IArglet {
+		private static final Matcher[] optionMatchers = {
+				/* "--target=" triple */
+				Pattern.compile("--target=" + REGEX_TARGET_DEFNITION).matcher("")};
+
+		/*-
+		* @see de.marw.cmake.cdt.lsp.IArglet#processArgs(java.lang.String)
+		*/
+		@Override
+		public int processArgument(IParseContext parseContext, IPath cwd, String argsLine) {
+			return processArgument(parseContext, argsLine, optionMatchers);
+		}
+	}
 
   ////////////////////////////////////////////////////////////////////
   /**
