fdac2a8c4901b234270bd872b40964221d69ee4d
15knots <11367029+15knots@users.noreply.github.com>
2023-11-10 21:14:13 +0100

    add help context for CMakePropertyTab

--- de.marw.cmake4eclipse.mbs.ui/src/de/marw/cmake4eclipse/mbs/ui/CMakePropertyTab.java
+++ de.marw.cmake4eclipse.mbs.ui/src/de/marw/cmake4eclipse/mbs/ui/CMakePropertyTab.java
@@ -77,7 +77,7 @@ public class CMakePropertyTab extends QuirklessAbstractCPropertyTab {
 
   @Override
   protected void createControls(final Composite parent) {
-    super.setHelpContextId(Activator.PLUGIN_ID + ".TODO");
+    super.setHelpContextId(Activator.PLUGIN_ID + ".cmake_options_tab_context");
     super.createControls(parent);
     usercomp.setLayout(new GridLayout(2, false));
 //    usercomp.setBackground(BACKGROUND_FOR_USER_VAR);
